dbfb031efeab23ad403caedec0e854f01380e698
THIS ONE IS A LITTLE BIT TRICKY KRUB <pattadon2546pn@gmail.com>
2023-08-31 17:54:32 +0700

    Completed tutorial part3.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from polls import views
+from polls.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include(views.index), name="index"),
     path("polls/", include("polls.urls")),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,18 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, Http404
 from django.urls import path
 from django.shortcuts import render
 from django.template import loader
 from .models import Question
+from django.shortcuts import get_object_or_404
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "index.html", context)
 
 def detail(request:HttpRequest, question_id)->HttpResponse:
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "detail.html", {"question": question})
 
 def results(request:HttpRequest, question_id)->HttpResponse:
     response = "You're looking at the results of question %s."
@@ -14,7 +21,3 @@ def results(request:HttpRequest, question_id)->HttpResponse:
 def vote(request:HttpRequest, question_id)->HttpResponse:
     return HttpResponse("You're voting on question %s." % question_id)
 
-def index(request:HttpRequest) -> render:
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "index.html", context)
