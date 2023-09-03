316fa4b1070bb6470c47b94ad6900a3e185d9f2d
Pattadon L <86756025+GGWPXXXX@users.noreply.github.com>
2023-09-03 11:56:04 +0700

    Adjusting the code to meet the guidelines.

--- polls/views.py
+++ polls/views.py
@@ -1,26 +1,44 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.shortcuts import render
-from .models import Question
+from django.shortcuts import render, get_object_or_404
+from .models import Question, Choice
 from django.utils import timezone
-from django.shortcuts import get_object_or_404
-from .models import Choice, Question
 from django.views import generic
 
 class IndexView(generic.ListView):
+    """
+    View for displaying the index page with the latest questions.
+    """
     template_name = "index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        "return the last 5 public question."
+        """
+        Return the last 5 public questions.
+        """
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
-
 class ResultsView(generic.DetailView):
+    """
+    View for displaying the results of a specific question.
+    """
     model = Question
     template_name = "results.html"
 
+class DetailView(generic.DetailView):
+    """
+    View for displaying the details of a specific question.
+    """
+    model = Question
+    template_name = "detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 def vote(request, question_id):
+    """
+    View for handling the voting process for a specific question.
+    """
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
@@ -33,14 +51,7 @@ def vote(request, question_id):
                 "error_message": "You didn't select a choice.",
             },
         )
-        
     else:
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-    
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = "detail.html"
-    def get_queryset(self):
-        return Question.objects.filter(pub_date__lte=timezone.now())
