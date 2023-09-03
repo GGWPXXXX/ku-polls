e3eb9df60d8436f781e720ce41113d55726dd648
Pattadon L <86756025+GGWPXXXX@users.noreply.github.com>
2023-09-03 11:53:36 +0700

    Adjusting the code to meet the guidlines.

--- polls/models.py
+++ polls/models.py
@@ -4,26 +4,59 @@ from django.utils import timezone
 from django.contrib import admin
 
 class Question(models.Model):
+    """
+    Represents a poll question.
+
+    Attributes:
+        question_text (str): The text of the question.
+        pub_date (datetime): The date and time when the question was published.
+    """
     question_text = models.CharField(max_length=100)
     pub_date = models.DateTimeField("date published", default=timezone.now)
-    
+
     def __str__(self) -> str:
+        """
+        Returns a string representation of the question text.
+
+        Returns:
+            str: The question text.
+        """
         return self.question_text
-    
+
     @admin.display(
         boolean=True,
         ordering="pub_date",
         description="Published recently?",
     )
     def was_published_recently(self):
+        """
+        Checks if the question was published recently.
+
+        Returns:
+            bool: True if the question was published within the last day, False otherwise.
+        """
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
+    """
+    Represents a choice for a poll question.
+
+    Attributes:
+        question (Question): The question to which this choice belongs.
+        choice_text (str): The text of the choice.
+        votes (int): The number of votes received for this choice.
+    """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)
-    
+
     def __str__(self) -> str:
+        """
+        Returns a string representation of the choice text.
+
+        Returns:
+            str: The choice text.
+        """
         return self.choice_text
