6ab6c7693eaa88be6e240265c84da82246db9b96
THIS ONE IS A LITTLE BIT TRICKY KRUB <pattadon2546pn@gmail.com>
2023-09-14 00:37:26 +0700

    Decorated webpages.

---
+++ polls/static/js/script.js
@@ -0,0 +1,25 @@
+
+const loginAlert = () =>{
+    alert("You need to login in order to vote!!")
+}
+
+
+// use for warn user when polls is closed in index.
+const disableLinks= () => {
+
+    const disabledLinks = document.querySelectorAll('.disabled');
+    disabledLinks.forEach(link => {
+        if (link.getAttribute('data-disabled') === 'true') {
+            link.addEventListener('click', (e) => {
+                e.preventDefault();
+                alert("Voting for this poll is closed.");
+            });
+        }
+    });
+    
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    disableLinks();
+});
